f8a7c5956c71325c9290623b37f94efdffe2c87d
tangjun <694756023@qq.com>
2018-11-04 10:57:24 +0800

    download

---
+++ app/src/main/java/com/jiaoyf/www/saas/download/DownloadListener.java
@@ -0,0 +1,13 @@
+package com.jiaoyf.www.saas.download;
+
+public interface DownloadListener {
+    void onProgress(int progress);
+
+    void onSuccess();
+
+    void onFailed();
+
+    void onPaused();
+
+    void onCanceled();
+}
